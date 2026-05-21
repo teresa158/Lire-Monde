cb4ded0a232d42bf48348ad8f4a9e3dcc0bbd129
teresa158 <wiamebaba2001@gmail.com>
2026-05-21 23:03:43 +0100

    générer les cartes des catégories

--- java.js
+++ java.js
@@ -1,10 +1,11 @@
 const state = {
   currentbook: null,
   books: [],
-  alire: []
+  alire: [],
+  genre: [],
 };
 
-let genre = document.getElementById("genre");
+let genrecont = document.getElementById("genre-container");
 let novels = document.getElementById("livres-containner");
 
 
@@ -13,10 +14,10 @@ let APIURL = "http://localhost:3000"
 
 async function fetshcategories() {
     try{
-        const response = await fetch(APIURL/categories);
+        const response = await fetch(`${APIURL}/categories`);
         if(!response.ok) throw new Error("Erreur serveur");
         const cat = await response.json()
-        state.alire = cat;
+        state.genre = cat;
     }catch (err){
         console.log("Impossible de charger, error")
     }
@@ -24,11 +25,25 @@ async function fetshcategories() {
 
 async function fetshlivres() {
     try{
-        const response = await fetch(APIURL/livres);
+        const response = await fetch(`${APIURL}/livres`);
         if(!response.ok) throw new Error("Erreur serveur");
         const livrs = await response.json()
-        state.books= livrs
+        state.books= livrs;
     }catch (err){
-        console.log("Impossible de charger, error")
+        console.log(err)
     }
+}
+
+function creatcategoriecard(){
+    state.genre.forEach(categorie => {
+        
+    let card = document.createElement("div");
+    card.className = "categorie-card";
+    card.innerHTML =  ` 
+    <img src="${categorie.image}">
+    <h3>${categorie.type}</h3>
+    `;
+    genrecont.appendChild(card);
+    });
+
 }
