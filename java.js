6384c44e5d7dce62d7ce8fc10f84ca965200e09b
teresa158 <wiamebaba2001@gmail.com>
2026-05-21 15:37:20 +0100

    initialisation du js et modifications generales en html et css

---
+++ java.js
@@ -0,0 +1,15 @@
+const state = {
+  currentbook: null,
+  books: [],
+  alire: []
+};
+
+let categories = document.getElementById("categories");
+
+
+let APIURL = "http://localhost:3000/livres";
+let apiurl = "http://localhost:3000/categories";
+
+async function fetshcategories() {
+    
+}
