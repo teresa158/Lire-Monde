eb1e08fe41f2039b8ddb2052cf6eb7e631d8ffe0
teresa158 <wiamebaba2001@gmail.com>
2026-05-21 22:25:56 +0100

    générer les fonctions fetsh

--- java.js
+++ java.js
@@ -4,14 +4,31 @@ const state = {
   alire: []
 };
 
-let categories = document.getElementById("categories");
-let livres = document.getElementById("livres");
+let genre = document.getElementById("genre");
+let novels = document.getElementById("livres-containner");
 
 
-let APIURL = "http://localhost:3000/livres";
-let apiurl = "http://localhost:3000/categories";
+let APIURL = "http://localhost:3000"
 
 
 async function fetshcategories() {
-    
+    try{
+        const response = await fetch(APIURL/categories);
+        if(!response.ok) throw new Error("Erreur serveur");
+        const cat = await response.json()
+        state.alire = cat;
+    }catch (err){
+        console.log("Impossible de charger, error")
+    }
+}
+
+async function fetshlivres() {
+    try{
+        const response = await fetch(APIURL/livres);
+        if(!response.ok) throw new Error("Erreur serveur");
+        const livrs = await response.json()
+        state.books= livrs
+    }catch (err){
+        console.log("Impossible de charger, error")
+    }
 }
