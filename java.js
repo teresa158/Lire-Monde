c842e74ce35d7d5c62672a4a82d6a2a1ceaab39b
teresa158 <wiamebaba2001@gmail.com>
2026-05-21 16:19:08 +0100

    l'ajout du package.json

--- java.js
+++ java.js
@@ -5,10 +5,12 @@ const state = {
 };
 
 let categories = document.getElementById("categories");
+let livres = document.getElementById("livres");
 
 
 let APIURL = "http://localhost:3000/livres";
 let apiurl = "http://localhost:3000/categories";
+
 
 async function fetshcategories() {
     
